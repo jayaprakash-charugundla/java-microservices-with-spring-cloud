66ed6b3d131273f3c8d142b22f02b6d0ca0af472
Jayaprakash Charugundla <jayaprakash.charugundla@blueyonder.com>
2022-01-10 22:58:56 +0530

    Configured Circuit Breaker

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ http://localhost:8081/dashboard?stationId=1002
 http://localhost:8082/customerdetails?fastpassid=800
 http://localhost:8082/customerdetails?fastpassid=801
 http://localhost:8082/customerdetails?fastpassid=802
+
+
+http://localhost:8081/actuator/circuitbreakers
+http://localhost:8081/actuator/circuitbreakerevents
--- tollrate-ui/src/main/java/com/jc/ps/DashboardController.java
+++ tollrate-ui/src/main/java/com/jc/ps/DashboardController.java
@@ -1,32 +1,47 @@
 package com.jc.ps;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Controller
 public class DashboardController {
 
-	@Autowired
-	private WebClient.Builder webClientBuilder;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @Autowired
+    private ReactiveCircuitBreakerFactory circuitBreakerFactory;
 
     @RequestMapping("/dashboard")
-	public String GetTollRate(@RequestParam(defaultValue = "1000") Integer stationId, Model m) {
+    public String GetTollRate(@RequestParam(defaultValue = "1000") Integer stationId, Model m) {
 
         //WebClient client = WebClient.create();
 
-        TollRate rate = webClientBuilder.build().get()
-            .uri("http://tollrate-service/tollrate/" + stationId)
-            .retrieve()
-            .bodyToMono(TollRate.class)
-            .block();
-		
-		System.out.println("stationId: " + stationId);
-		m.addAttribute("rate", rate);
-		return "dashboard";
-	}
-    
+        ReactiveCircuitBreaker rcb = circuitBreakerFactory.create("tollrate-cb");
+
+        Mono<TollRate> rate = rcb.run(webClientBuilder.build().get()
+                .uri("http://tollrate-service/tollrate/" + stationId)
+                .retrieve()
+                .bodyToMono(TollRate.class),
+            throwable -> getDefaultTollRate());
+
+        System.out.println("stationId: " + stationId);
+        m.addAttribute("rate", rate.block());
+        return "dashboard";
+    }
+
+    private Mono<TollRate> getDefaultTollRate() {
+        log.info("Fallback method called");
+        return Mono.just(new TollRate(0, 2.00f, ""));
+    }
+
 }
